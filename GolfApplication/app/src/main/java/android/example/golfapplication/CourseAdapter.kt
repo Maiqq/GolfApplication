79be5609999b9c1064b5bf73289660650f80d79d
MikaelFagerstrom <mikael.fagerstrom@edu.novia.fi>
2019-05-20 16:30:04 +0300

    Exercise 17 about halfway done

---
+++ GolfApplication/app/src/main/java/android/example/golfapplication/CourseAdapter.kt
@@ -0,0 +1,60 @@
+package android.example.golfapplication
+
+import android.content.Intent
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import kotlinx.android.synthetic.main.recycler_view_item.view.*
+import org.json.JSONObject
+
+class CourseAdapter (
+    private val parentActivity: MainActivity
+) : RecyclerView.Adapter<CourseAdapter.ViewHolder>() {
+
+
+    companion object {
+        var position: Int = 0
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CourseAdapter.ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.recycler_view_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    // return item count in employees
+    override fun getItemCount(): Int = MainActivity.course.length()
+
+    // bind data to UI View Holder
+    override fun onBindViewHolder(holder: CourseAdapter.ViewHolder, position: Int) {
+        // employee to bind UI
+        val course: JSONObject = MainActivity.course.getJSONObject(position)
+        // name
+        holder.nameTextView.text = course["course"].toString()
+        holder.addressTextView.text = course["address"].toString()
+        // email
+        holder.emailTextView.text = course["email"].toString()
+        // phone
+        holder.phoneTextView.text = course["phone"].toString()
+        // department
+        // image
+
+        Glide.with(holder.imageView.context).load("http://ptm.fi/materials/golfcourses/" + course["image"]).apply(RequestOptions.circleCropTransform()).into(holder.imageView)
+    }
+
+    // View Holder class to hold UI views
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val nameTextView: TextView = view.nameTextView
+        val addressTextView: TextView = view.addressTextView
+        val emailTextView: TextView = view.emailTextView
+        val phoneTextView: TextView = view.phoneTextView
+        val imageView: ImageView = view.imageView
+    }
+}
---
+++ GolfApplication/app/src/main/java/android/example/golfapplication/MainActivity.kt
@@ -0,0 +1,86 @@
+package android.example.golfapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
+import android.support.v7.widget.ToolbarWidgetWrapper
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.support.design.widget.CollapsingToolbarLayout
+
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
+
+import org.json.JSONArray
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        var course: JSONArray = JSONArray()
+    }
+
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+            val mToolbar:Toolbar = toolbar
+            setSupportActionBar(mToolbar)
+
+        if(course.length()==0) loadGolfCoursesJsonData()
+            else setupRecyclerView(course)
+
+            val collapsingToolbar = findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout)
+            collapsingToolbar.title = "SGKY - Kentät"
+
+
+
+        }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    // Add layout manager and adapter to recycler view
+    private fun setupRecyclerView(course: JSONArray) {
+        recylerView.layoutManager = LinearLayoutManager(this)
+        recylerView.adapter = CourseAdapter(this)
+    }
+
+    // Load JSON from the net
+    private fun loadGolfCoursesJsonData() {
+        // Instantiate the RequestQueue
+        val queue = Volley.newRequestQueue(this)
+        // URL to JSON data
+        val url = "http://ptm.fi/materials/golfcourses/golf_courses.json"
+        // Create request and listeners
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                course = response.getJSONArray("courses")
+                setupRecyclerView(course)
+            },
+            Response.ErrorListener { error ->
+                Log.d("JSON",error.toString())
+            }
+        )
+        // Add the request to the RequestQueue.
+        queue.add(jsonObjectRequest)
+
+    }
+    }
+
+
+
+
+
